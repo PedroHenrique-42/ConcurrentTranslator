81713d9bcf9d21b10edd9c6ac4e06d87a14a30d0
Pedro Henrique <phlferezin03@gmail.com>
2026-04-23 08:05:09 -0300

    feat: criando camada de API para realizar chamadas HTTP

---
+++ app/src/main/java/br/com/pedroferezin/concurrenttranslator/api/ConcurrentTranslatorApiClient.kt
@@ -0,0 +1,15 @@
+package br.com.pedroferezin.concurrenttranslator.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ConcurrentTranslatorApiClient {
+    private const val BASE_URL = "https://deep-translate1.p.rapidapi.com/language/translate/v2"
+
+    private val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(
+        GsonConverterFactory.create()
+    ).build()
+
+    val service: ConcurrentTranslatorApiService =
+        retrofit.create(ConcurrentTranslatorApiService::class.java)
+}
