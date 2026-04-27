6d73e7940a870b16e111f10edf9aea087addaf5f
Pedro Henrique <phlferezin03@gmail.com>
2026-04-27 09:01:50 -0300

    feat: configurando listeners na activity para definir o conteúdo dos spinners

--- app/src/main/java/br/com/pedroferezin/concurrenttranslator/ui/MainActivity.kt
+++ app/src/main/java/br/com/pedroferezin/concurrenttranslator/ui/MainActivity.kt
@@ -1,18 +1,97 @@
 package br.com.pedroferezin.concurrenttranslator.ui
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import br.com.pedroferezin.concurrenttranslator.R
 import br.com.pedroferezin.concurrenttranslator.databinding.ActivityMainBinding
+import br.com.pedroferezin.concurrenttranslator.ui.viewmodels.ConcurrentTranslatorViewModel
+import br.com.pedroferezin.concurrenttranslator.ui.viewmodels.states.FetchLanguagesState
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val concurrentTranslatorViewModel: ConcurrentTranslatorViewModel by viewModels()
+
+    private var selectedOriginLanguage: String = ""
+    private var selectedDestinyLanguage: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
         setSupportActionBar(amb.mainTb.apply { title = getString(R.string.app_name) })
+
+        val languagesAdapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, mutableListOf<String>())
+
+        listenFetchState(languagesAdapter)
+        configureViewListeners(languagesAdapter)
+
+        concurrentTranslatorViewModel.fetchLanguagues()
+    }
+
+    private fun configureViewListeners(adapter: ArrayAdapter<String>) {
+        with(amb) {
+            originLanguageAc.apply {
+                setAdapter(adapter)
+                setOnClickListener { selectedOriginLanguage = text.toString() }
+            }
+
+            destinyLanguageAc.apply {
+                setAdapter(adapter)
+                setOnClickListener { selectedDestinyLanguage = text.toString() }
+            }
+
+            translateBt.setOnClickListener {
+                concurrentTranslatorViewModel.translate(
+                    originLanguageEt.text.toString(),
+                    selectedOriginLanguage,
+                    selectedDestinyLanguage
+                )
+            }
+        }
+    }
+
+    fun listenFetchState(adapter: ArrayAdapter<String>) {
+        lifecycleScope.launch {
+            concurrentTranslatorViewModel.fetchLanguagesState.collect { state ->
+                when (state) {
+                    is FetchLanguagesState.Empty -> {
+                        adapter.clear()
+                    }
+
+                    is FetchLanguagesState.Success -> {
+                        adapter.clear()
+                        adapter.addAll(state.languages.languages.map { language -> language.name }
+                            .sorted())
+
+                        adapter.getItem(0)?.also { language ->
+                            amb.originLanguageAc.setText(language, false)
+                            selectedOriginLanguage = language
+                        }
+
+                        adapter.getItem(adapter.count - 1)?.also { language ->
+                            amb.destinyLanguageAc.setText(language, false)
+                            selectedOriginLanguage = language
+                        }
+                    }
+
+                    is FetchLanguagesState.Error -> {
+                        adapter.clear()
+                        Snackbar.make(
+                            amb.root,
+                            state.message,
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
     }
 }
