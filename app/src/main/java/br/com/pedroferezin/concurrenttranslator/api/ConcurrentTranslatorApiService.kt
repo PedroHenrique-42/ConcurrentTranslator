333991c846d7310da878e569a0460e5e38c2037b
Pedro Henrique <phlferezin03@gmail.com>
2026-04-29 20:35:09 -0300

    fix: arrumando as URLs das APIs

--- app/src/main/java/br/com/pedroferezin/concurrenttranslator/api/ConcurrentTranslatorApiService.kt
+++ app/src/main/java/br/com/pedroferezin/concurrenttranslator/api/ConcurrentTranslatorApiService.kt
@@ -5,6 +5,7 @@ import br.com.pedroferezin.concurrenttranslator.domain.TranslationResult
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ConcurrentTranslatorApiService {
@@ -12,14 +13,14 @@ interface ConcurrentTranslatorApiService {
         "x-rapidapi-host: deep-translate1.p.rapidapi.com",
         "x-rapidapi-key: f8d372a6f0msh14f81a14120937dp19f03bjsn6526a833dcb4"
     )
-    @GET("languages")
+    @GET("language/translate/v2/languages")
     fun fetchLanguagues(): Call<LanguagesList>
 
     @Headers(
         "x-rapidapi-host: deep-translate1.p.rapidapi.com",
         "x-rapidapi-key: f8d372a6f0msh14f81a14120937dp19f03bjsn6526a833dcb4"
     )
-    @GET()
+    @POST("language/translate/v2")
     fun translate(
         @Query("q") text: String,
         @Query("source") originLanguague: String,
